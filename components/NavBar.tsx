f451b36fb8772601849006d4b4d69e66e9ab556d
dorkydhruv <thedhruvsharma@gmail.com>
2024-07-27 21:31:36 +0500

    init

---
+++ components/NavBar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="logo"
+          width={40}
+          height={40}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Vrrom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">{/* Clerk Management */}</div>
+      {/* MobileNav */}
+      <MobileNav />
+    </nav>
+  );
+};
+
+export default NavBar;
